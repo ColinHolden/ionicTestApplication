3d884643a6786666f0863db40cc31fe7ef0b50f2
Colin Holden <colin.holden@derivco.co.za>
2018-02-05 10:52:17 +0200

    Added Server Side Code
    Web Api Project

---
+++ WEBApiResponses/WEBApiResponses/Models/About.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WEBApiResponses.Models
+{
+  [RoutePrefix("About")]
+  public class About : ApiController
+  {
+    private const string AboutIonic = "This is a an application that makes use of the ionic framework, which is integrated with typescript and sass for a great user developer experience";
+
+    [Route("Info")]
+    public IHttpActionResult GetMainText()
+    {
+      return Ok(new
+      {
+        data = AboutIonic  
+      });
+    }
+  }
+}
---
+++ WEBApiResponses/WEBApiResponses/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WEBApiResponses.WebApiApplication" Language="C#" %>
